a1ea0a3dd1c57ad74d7fb24cdad255f4edc9ab8e
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-12-21 12:48:58 -0300

    21/12/2023

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
+
+import Login from './components/Auth/Login';
+import Profile from './components/Profile/Profile';
+import Feed from './components/Feed/Feed';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav className="navbar">
+          <Link to="/">Inicio</Link>
+          <Link to="/login">Iniciar sesión</Link>
+          <Link to="/profile">Perfil</Link>
+          <Link to="/feed">Feed</Link>
+        </nav>
+
+        <div className="container">
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/feed" component={Feed} />
+            <Route path="/" exact>
+              <h2>Bienvenido a la Página de Inicio</h2>
+              <p>Selecciona una opción del menú de navegación.</p>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
